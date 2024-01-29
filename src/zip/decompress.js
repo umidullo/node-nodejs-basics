92011941cea659b122975d2097caf9c80c574fc6
umidullo <suyunovumidullo@gmail.com>
2024-01-29 23:47:06 +0500

    feat: add compress and decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import zlib from "zlib";
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGunzip();
+  const source = fs.createReadStream(__dirname + "/files/archive.gz");
+  const destination = fs.createWriteStream(
+    __dirname + "/files/fileToCompress.txt"
+  );
+
+  source.pipe(gzip).pipe(destination);
 };
 
 await decompress();
