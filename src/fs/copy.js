a88058ab3eaa70171241362a943c8cdc555cf285
umidullo <suyunovumidullo@gmail.com>
2024-01-29 16:23:13 +0500

    feat: add fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const targetFolder = __dirname + "/files";
+const newFolder = __dirname + "/files_copy";
+
 const copy = async () => {
-    // Write your code here 
+  const targetFolderExist = fs.existsSync(targetFolder);
+  const newFolderExist = fs.existsSync(newFolder);
+
+  if (targetFolderExist && !newFolderExist) {
+    fs.mkdirSync(newFolder);
+    fs.readdir(targetFolder, (err, files) => {
+      for (let file of files) {
+        fs.copyFile(
+          targetFolder + "/" + file,
+          newFolder + "/" + file,
+          (err) => {}
+        );
+      }
+    });
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
