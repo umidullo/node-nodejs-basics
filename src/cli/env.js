8f2f8f10127e76ac316ec0dee847776cb43e3f46
umidullo <suyunovumidullo@gmail.com>
2024-01-29 17:01:31 +0500

    feat: add cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  console.log(
+    Object.entries(process.env)
+      .filter((arr) => arr[0].includes("RSS_"))
+      .map((arr) => `${arr[0]}=${arr[1]}`)
+      .join("; ")
+  );
 };
 
 parseEnv();
