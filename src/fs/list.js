66bc2cfd090f8e6d61d4ff49cc04edc95e530cba
umidullo <suyunovumidullo@gmail.com>
2024-01-29 16:09:24 +0500

    feat: add fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const path = __dirname + "/files";
+
 const list = async () => {
-    // Write your code here 
+  if (fs.existsSync(path)) {
+    fs.readdir(path, (err, files) => {
+      console.log(files);
+    });
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
