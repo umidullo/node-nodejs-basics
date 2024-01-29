e5d21dd3d57122260c675a58582ce02503638a33
umidullo <suyunovumidullo@gmail.com>
2024-01-29 11:23:57 +0500

    feat: add fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  fs.readFile(__dirname + "/files/fileToRead.txt", "utf8", (error, data) => {
+    if (error) {
+      throw new Error("FS operation failed");
+    } else {
+      console.log(data);
+    }
+  });
 };
 
 await read();
