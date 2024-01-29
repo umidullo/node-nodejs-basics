dbe126e164c8d464f3f0aa13970b4b4332342a25
umidullo <suyunovumidullo@gmail.com>
2024-01-29 11:23:42 +0500

    feat: add fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const content = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  if (fs.existsSync(__dirname + "/files/fresh.txt")) {
+    throw new Error("FS operation failed");
+  } else {
+    fs.writeFile(__dirname + "/files/fresh.txt", content, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 };
 
 await create();
