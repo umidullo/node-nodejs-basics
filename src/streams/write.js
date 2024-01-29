a03e5fe058b8f0b7d3bde42208598360b3ee532a
umidullo <suyunovumidullo@gmail.com>
2024-01-29 23:01:48 +0500

    feat: add streams write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const ws = fs.createWriteStream(__dirname + "/files/fileToWrite.txt");
+
+  process.stdin.on("data", (chunk) => {
+    ws.write(chunk);
+  });
+
+  process.stdin.on("end", () => {
+    ws.end();
+  });
 };
 
 await write();
