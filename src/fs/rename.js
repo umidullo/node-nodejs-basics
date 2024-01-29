2e5a3c904c304f1ad94b4ec6b3832c7df9023be4
umidullo <suyunovumidullo@gmail.com>
2024-01-29 15:44:07 +0500

    feat: add fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const oldName = __dirname + "/files/wrongFilename.txt";
+const newName = __dirname + "/files/properFilename.md";
+
 const rename = async () => {
-    // Write your code here 
+  const wrongFilExist = fs.existsSync(oldName);
+  const newFileDoesNotExist = fs.existsSync(newName);
+
+  if (wrongFilExist && !newFileDoesNotExist) {
+    fs.rename(oldName, newName, (err) => {});
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
