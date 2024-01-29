a16a4e80e1d8018f6ec945001601497261abbbdd
umidullo <suyunovumidullo@gmail.com>
2024-01-29 20:46:16 +0500

    feat: add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import crypto from "crypto";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const input = fs.createReadStream(
+    __dirname + "/files/fileToCalculateHashFor.txt"
+  );
+  const hash = crypto.createHash("sha256");
+  hash.setEncoding("hex");
+
+  input.on("end", function () {
+    hash.end();
+    console.log(hash.read());
+  });
+
+  input.pipe(hash);
 };
 
 await calculateHash();
